b93397d366882759abc0161ce4984b6f52b2aa08
rrouwelng <rouwelngacha@gmail.com>
2026-05-28 13:34:26 +0300

    Feat : Changed the Normal users model to User model because of auth user login conflixt, cannot have 2 user models

--- traceblocks/tracker/models.py
+++ traceblocks/tracker/models.py
@@ -3,10 +3,27 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 
-class NormalUsers(AbstractUser):
-    first_name = models.CharField(max_length=100, blank=False)
-    last_name = models.CharField(max_length=100, blank=False)
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    TYPE_NORMAL = 'NORMAL'
+    TYPE_ORG = 'ORGANISATION'
+    
+    USER_TYPE_CHOICES = [
+        (TYPE_NORMAL, 'Normal User'),
+        (TYPE_ORG, 'Organisation User'),
+    ]
+
+    user_type = models.CharField(
+        max_length=20, 
+        choices=USER_TYPE_CHOICES, 
+        default=TYPE_NORMAL
+    )
+    
+    #Fields for Normal users 
     middle_name = models.CharField(max_length=100, blank=True)
-    phonenumber = models.CharField(max_length=15)
-
-
+    phonenumber = models.CharField(max_length=15, blank=True, null=True)
+    
+    #Fields for Organisation users
+    organisation_name = models.CharField(max_length=100, blank=True, null=True)
