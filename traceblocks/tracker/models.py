09430522f36e5eefcc4fe49f2c6fa05ee2a28151
rrouwelng <rouwelngacha@gmail.com>
2026-05-28 13:11:37 +0300

    Feat : Created the user models, that will be migrated to the database to create a table

--- traceblocks/tracker/models.py
+++ traceblocks/tracker/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class NormalUsers(AbstractUser):
+    first_name = models.CharField(max_length=100, blank=False)
+    last_name = models.CharField(max_length=100, blank=False)
+    middle_name = models.CharField(max_length=100, blank=True)
+    phonenumber = models.CharField(max_length=15)
+
+
