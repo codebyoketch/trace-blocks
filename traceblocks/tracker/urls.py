bd7a1be54ceb2285402408500630faa1a9238a61
odingaval <odingaval71@gmail>
2026-05-29 06:54:53 +0300

    Feat : DEleted the original views merged from the rrouwelng branch. became obslete

--- traceblocks/tracker/urls.py
+++ traceblocks/tracker/urls.py
@@ -4,21 +4,14 @@ from django.contrib.auth import views as auth_views
  
 urlpatterns = [
     path("index",                              views.index,            name="index"),
-    path("",                        auth_views.LoginView.as_view(template_name="login.html"), name="login"),
+    path("login/",                        auth_views.LoginView.as_view(template_name="login.html"), name="login"),
     path("logout/",                       auth_views.LogoutView.as_view(next_page="index"), name="logout"),
     path("products/new/",                 views.create_product,   name="create_product"),
     path("products/<str:sku>/",           views.product_detail,   name="product_detail"),
     path("products/events/",    views.add_event,        name="add_event"),
     path("events/<int:event_id>/status/", views.refresh_tx_status,name="refresh_tx_status"),
     path("interface/",                    views.interface_view,   name="interface"),
-    path("new/event/", views.events_view, name="events")
+    path("new/event/", views.events_view, name="events"),
+    path("", views.CreateUser_view, name="createuser")
 ]
  
-from django.contrib.auth import views as auth_views
-from . import views
-
-urlpatterns = [
-    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('account/', views.profile_view, name ='profile')
-]
